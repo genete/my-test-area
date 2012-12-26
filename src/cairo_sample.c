5a27830586947f14e939374460eb762eaaeb7bb2
Carlos Lopez <genetita@gmail.com>
2012-12-26 12:27:26 +0100

    Several changes: Rename cairo2.c to cairo_sample.c Add function to create cairo_device_t based on Display and GLXContext Modify on_expose_event to create a gl context and pass it to the gl surface creation. Add on_configure event handler. Add Cairo include dirs, library dirs and libraries names.

--- src/cairo_sample.c
+++ src/cairo_sample.c
@@ -1,23 +1,72 @@
 #include <stdio.h>
 #include <cairo.h>
+#include <cairo-gl.h>
 #include <gtk/gtk.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <gdk/gdkx.h>
 
 
 double coordx[100];
 double coordy[100];
 
 int count = 0;
+char buff[60];
+
+gboolean rendering=FALSE;
+
+
+static cairo_device_t *
+cairo_sample_gl_context_create(Display *dpy, GLXContext *out_gl_ctx)
+{
+    int attribs[] = { GLX_RGBA,
+		GLX_RED_SIZE, 1,
+		GLX_GREEN_SIZE, 1,
+		GLX_BLUE_SIZE, 1,
+		GLX_DOUBLEBUFFER,
+		GLX_DEPTH_SIZE, 1,
+		None };
+    GLXContext gl_ctx;
+    XVisualInfo *visinfo;
+    cairo_device_t *ctx;
+	
+    visinfo = glXChooseVisual (dpy, DefaultScreen (dpy), attribs);
+    if (visinfo == NULL) {
+		fprintf(stderr, "Failed to create RGB, double-buffered visual\n");
+		return NULL;
+    }
+	
+    gl_ctx = glXCreateContext (dpy, visinfo, NULL, True);
+    *out_gl_ctx = gl_ctx;
+	
+    XFree(visinfo);
+	
+    ctx = cairo_glx_device_create (dpy, gl_ctx);
+	
+    return ctx;
+}
+
 
 static gboolean
 on_expose_event(GtkWidget *widget,
     GdkEventExpose *event,
     gpointer data)
 {
-	cairo_t *cr;
+	if(rendering)
+		return TRUE;
+	rendering=TRUE;
 	
-	cr = gdk_cairo_create(widget->window);
+	cairo_t *cr;
+	GLXContext gl_ctx;
+	
+	cairo_surface_t* window_surface;
+	cairo_device_t *ctx = NULL;
+	ctx=cairo_sample_gl_context_create(GDK_WINDOW_XDISPLAY(gtk_widget_get_window(widget)), &gl_ctx);
+	window_surface=cairo_gl_surface_create_for_window(ctx, GDK_WINDOW_XID(gtk_widget_get_window(widget)), 480, 480);
+	cr = cairo_create(window_surface);
 	
 	cairo_save(cr);
+	cairo_reset_clip(cr);
 	cairo_set_source_rgb(cr, 0, 0, 0);
 	cairo_set_line_width (cr, 0.5);
 	cairo_set_antialias(cr, CAIRO_ANTIALIAS_SUBPIXEL);
@@ -34,18 +83,29 @@ on_expose_event(GtkWidget *widget,
 
 	cairo_set_source_rgb(cr, 0.9, 0, 0);
 	cairo_move_to(cr, 10, 10);
-	GtkWindow *w=GTK_WINDOW(widget);
-	gint x, y;
-	gtk_window_get_position(w, &x, &y);
-	char buff[60];
-	sprintf(buff,"x=%d; y=%d", x, y);
 	cairo_show_text(cr, buff);
 	cairo_destroy(cr);
 
 	if(count > 99) count=0;
 	
+	rendering=FALSE;
+	
 	return FALSE;
 }
+
+
+gboolean
+on_configure_event(GtkWidget *w,
+						GdkEvent *event, gpointer data)
+{
+	gint x, y;
+	x = event->configure.x;
+	y = event->configure.y;
+	sprintf(buff,"x=%d; y=%d", x, y);
+	gtk_widget_queue_draw(w);
+	return FALSE;
+}
+
 
 gboolean clicked(GtkWidget *widget, GdkEventButton *event,
     gpointer user_data)
@@ -73,7 +133,7 @@ int main (int argc, char *argv[])
   
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
  
-  gtk_widget_add_events (window, GDK_BUTTON_PRESS_MASK);
+  gtk_widget_add_events (window, GDK_BUTTON_PRESS_MASK|GDK_CONFIGURE);
 
   g_signal_connect(window, "expose-event",
       G_CALLBACK(on_expose_event), NULL);
@@ -81,12 +141,13 @@ int main (int argc, char *argv[])
       G_CALLBACK(gtk_main_quit), NULL);
   g_signal_connect(window, "button-press-event", 
       G_CALLBACK(clicked), NULL);
- 
+  g_signal_connect(window, "configure-event",
+					 G_CALLBACK(on_configure_event), NULL);
+
   gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-  gtk_window_set_title(GTK_WINDOW(window), "lines");
+  gtk_window_set_title(GTK_WINDOW(window), "linese");
   gtk_window_set_default_size(GTK_WINDOW(window), 400, 300); 
   gtk_widget_set_app_paintable(window, TRUE);
-
   gtk_widget_show_all(window);
 
   gtk_main();
