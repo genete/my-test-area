3870c0d8bb5b6ff8f6c1c5e7a3025e3b04e51952
Carlos Lopez <genetita@gmail.com>
2013-01-10 18:23:55 +0100

    Add time consuming drawing operations

--- src/cairo_sample.c
+++ src/cairo_sample.c
@@ -14,7 +14,7 @@
 static GdkPixmap *pixmap = NULL;
 
 static gboolean rendering=FALSE;
-cairo_surface_t* window_surface=NULL;
+//cairo_surface_t* window_surface=NULL;
 
 struct closure {
     Display *dpy;
@@ -94,50 +94,40 @@ create_source_surface(int width, int height)
 }
 
 
-void do_draw()
-{
-
-}
-
-static gboolean
-on_expose_event(GtkWidget *widget,
-    GdkEventExpose *event,
-    gpointer data)
+void do_draw(void *ptr)
 {
     int width, height;
-	
-	if(rendering)
-		return TRUE;
+
 	rendering=TRUE;
-		
 	gdk_drawable_get_size(pixmap, &width, &height);
 	//create a gtk-independant surface to draw on
     cairo_surface_t *cst = create_source_surface(width, height);
+	//cairo_surface_t* cst = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
     cairo_t *cr = cairo_create(cst);
-
+	
 	//cr = cairo_create(window_surface);
 	cairo_status_t status;
 	status=cairo_status(cr);
 	if(status)
-		return FALSE;
-		
+		return;
+	
 	cairo_save(cr);
 	cairo_set_source_rgb(cr, 1, 1, 1);
 	cairo_paint(cr);
 	cairo_restore(cr);
 	
 	cairo_save(cr);
-	cairo_set_source_rgb(cr, 0.9, 0, 0);
-	cairo_rectangle(cr, 10, 10, 50, 80);
-	cairo_fill(cr);
-	cairo_restore(cr);
-	
-	cairo_save(cr);
 	cairo_set_source_rgb(cr, 0, .5, 0.2);
 	cairo_set_line_width (cr, 0.5);
 	cairo_set_antialias(cr, CAIRO_ANTIALIAS_SUBPIXEL);
-	cairo_move_to(cr, 0, 0);
-	cairo_line_to(cr, 110, 20);
+//	cairo_set_operator(cr, CAIRO_OPERATOR_DEST_OUT);
+	for(int j=0;j<20;j++)
+	{
+		for(int i=0; i<100; i++)
+		{
+			cairo_line_to(cr, rand()%width, rand()%height);
+		}
+	}
 	cairo_stroke(cr);
 	cairo_restore(cr);
 	cairo_destroy(cr);
@@ -146,6 +136,17 @@ on_expose_event(GtkWidget *widget,
     cairo_set_source_surface (cr_pixmap, cst, 0, 0);
     cairo_paint(cr_pixmap);
     cairo_destroy(cr_pixmap);
+
+	rendering=FALSE;
+
+}
+
+static gboolean
+on_expose_event(GtkWidget *widget,
+    GdkEventExpose *event,
+    gpointer data)
+{
+	do_draw(NULL);
 	
 	gdk_draw_drawable(widget->window,
 					  widget->style->fg_gc[GTK_WIDGET_STATE(widget)], pixmap,
@@ -154,19 +155,12 @@ on_expose_event(GtkWidget *widget,
 					  event->area.x, event->area.y,
 					  event->area.width, event->area.height);
 
-	rendering=FALSE;
     return TRUE;
-	
-	/*Transfer drawings to screen*/
-	//cairo_gl_surface_swapbuffers (window_surface);
-	
-	
-//	return FALSE;
 }
 
 void on_destroy()
 {
-	cairo_surface_destroy(window_surface);
+//	cairo_surface_destroy(window_surface);
 	gtk_main_quit();
 }
 
